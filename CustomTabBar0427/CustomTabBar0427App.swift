26da15c345472dee745beec896b197666a115064
张亚飞 <672233580@qq.com>
2022-04-27 14:45:34 +0800

    Initial Commit

---
+++ CustomTabBar0427/CustomTabBar0427App.swift
@@ -0,0 +1,17 @@
+//
+//  CustomTabBar0427App.swift
+//  CustomTabBar0427
+//
+//  Created by 张亚飞 on 2022/4/27.
+//
+
+import SwiftUI
+
+@main
+struct CustomTabBar0427App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
